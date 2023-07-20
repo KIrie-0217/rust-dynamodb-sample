fcf38a486263c69e0a293e70b457c56f1c0bc9d2
KIrie-0217 <kook.cuckoo@gmail.com>
2023-07-21 01:27:27 +0900

    deploy done

--- rust_app/src/main.rs
+++ rust_app/src/main.rs
@@ -1,29 +1,12 @@
 use aws_sdk_dynamodb::types::AttributeValue;
-use aws_sdk_dynamodb::Client;
-use lambda_runtime::{run, service_fn, Error, LambdaEvent};
+use aws_sdk_dynamodb::{Client, Error as OtherError};
+use lambda_http::{run, service_fn, Body, Error, Request, Response};
 use serde::{Deserialize, Serialize};
-use std::env;
-use std::fmt::Debug;
+use tracing::info;
 
-/// This is a made-up example. Requests come into the runtime as unicode
-/// strings in json format, which can map to any structure that implements `serde::Deserialize`
-/// The runtime pays no attention to the contents of the request payload.
-#[derive(Deserialize)]
-struct Request {}
-
-/// This is a made-up example of what a response structure may look like.
-/// There is no restriction on what it can be. The runtime requires responses
-/// to be serialized into json. The runtime pays no attention
-/// to the contents of the response payload.
-#[derive(Serialize)]
-struct Response {
-    statusCode: i32,
-    body: String,
-}
-
-#[derive(Deserialize, Serialize, Debug)]
-struct Account {
-    account_id: u16,
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct Account {
+    account_id: String,
     account_name: String,
     password: String,
 }
@@ -33,13 +16,38 @@ struct Account {
 /// There are some code example in the following URLs:
 /// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
 /// - https://github.com/aws-samples/serverless-rust-demo/
-async fn function_handler(event: LambdaEvent<Request>) -> Result<Response, Error> {
-    // Prepare the response
-    let resp = Response {
-        statusCode: 200,
-        body: "Hello World!".to_string(),
+async fn function_handler(
+    dynamodb_client: &Client,
+    event: Request,
+) -> Result<Response<Body>, Error> {
+    let body = event.body();
+    let body_str = std::str::from_utf8(body).expect("invalid body");
+
+    info!(payload = %body_str,"Json Payload received");
+
+    let account = match serde_json::from_str::<Account>(body_str) {
+        Ok(account) => account,
+        Err(err) => {
+            let resp = Response::builder()
+                .status(400)
+                .header("content-type", "text/html")
+                .body(err.to_string().into())
+                .map_err(Box::new)?;
+            return Ok(resp);
+        }
     };
 
+    // Insert into the table.
+    add_account(dynamodb_client, account.clone(), "rust-lambda-example").await?;
+
+    // Desirialize into json to return in the Responese
+    let body_out = serde_json::to_string(&account)?;
+
+    let resp = Response::builder()
+        .status(200)
+        .header("content-type", "text/html")
+        .body(body_out.into())
+        .map_err(Box::new)?;
     // Return `Response` (it will be serialized to JSON automatically by the runtime)
     Ok(resp)
 }
@@ -54,5 +62,39 @@ async fn main() -> Result<(), Error> {
         .without_time()
         .init();
 
-    run(service_fn(function_handler)).await
+    info!("Getting DynamoDB client");
+    let config = ::aws_config::load_from_env().await;
+    let client = Client::new(&config);
+
+    run(service_fn(|event: Request| async {
+        function_handler(&client, event).await
+    }))
+    .await
+}
+
+// Add an item into a table.
+pub async fn add_account(
+    dynamodb_client: &Client,
+    account: Account,
+    table: &str,
+) -> Result<(), OtherError> {
+    // Set Attribute
+    let account_id = AttributeValue::N(account.account_id);
+    let account_name = AttributeValue::S(account.account_name);
+    let password = AttributeValue::S(account.password);
+
+    // Create request
+    let request = dynamodb_client
+        .put_item()
+        .table_name(table)
+        .item("account_id", account_id)
+        .item("account_name", account_name)
+        .item("password", password);
+
+    info!("adding item to DynamoDB");
+
+    // send
+    let _resp = request.send().await?;
+
+    Ok(())
 }
